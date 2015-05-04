c40866ade386ac844179af4b646a2c9a8fba9d6d
Umur Gedik <umurgdk@gmail.com>
2015-05-04 11:55:15 +0300

    deployment updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,13 +39,13 @@ namespace :deploy do
 
   task :restart do
     on roles(:web) do
+      execute("sh #{deploy_to}/setenv.sh")
       capture("#{deploy_to}/bin/restart")
     end
   end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      execute("sh #{deploy_to}/setenv.sh")
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
